43f52eacfe9a644b818cdf693b6d465229ae58ba
Gustavo Mamede <gustavo@vestelivre.com.br>
2025-07-25 00:07:05 -0300

    Adiciona rota de setup temporária 2

--- tudomixsite/urls.py
+++ tudomixsite/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from loja.views import setup_site
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('loja.urls'))
+    path('', include('loja.urls')),
+    path('setup/', setup_site),
 ]
