c970447d417b28d09938ff46ec2e47bec788cb13
Gustavo Mamede <gustavo@vestelivre.com.br>
2025-07-26 14:10:18 -0300

    remover c.superuser

--- tudomixsite/urls.py
+++ tudomixsite/urls.py
@@ -18,10 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from loja.views import criar_superuser_temporario
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('loja.urls')),
-    path('criaradmin/', criar_superuser_temporario),
 ]
